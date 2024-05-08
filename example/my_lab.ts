e80f98ddaef9d373c6d01212e356b990b96f5309
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-05-08 09:58:13 -0700

    Codegen (#3)
    * Create codegen.ts
    
    * codegen moment
    
    * create lab for generating other labs
    
    * generate codegen lab
    
    * add titles to example notes

---
+++ example/my_lab.ts
@@ -0,0 +1,8 @@
+// Generated lab file. DO NOT MODIFY.
+import { Lab } from "labs/labs.ts";
+import { linksLab } from "labs/links.ts"
+import { notesLab } from "labs/notes.ts"
+
+export const myLab = new Lab()
+  .extend(notesLab)
+  .extend(linksLab);
---
+++ gen.ts
@@ -0,0 +1,2 @@
+//deno:generate deno run --allow-write codegen/codegen.ts
+//deno:generate deno run --allow-write example/codegen.ts
