8356ea7946271f8619c9076d3035fa9ba8869f87
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-05-10 13:53:04 -0700

    generateImport: include semicolons

--- example/my_lab.ts
+++ example/my_lab.ts
@@ -1,7 +1,7 @@
 // Generated lab file. DO NOT MODIFY.
 import { Lab } from "labs/labs.ts";
-import { linksLab } from "labs/links.ts"
-import { notesLab } from "labs/notes.ts"
+import { linksLab } from "labs/links.ts";
+import { notesLab } from "labs/notes.ts";
 
 export const myLab = new Lab()
   .extend(notesLab)
