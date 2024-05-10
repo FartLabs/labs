909a9846c9ca4460d1a2ff6dcb4ae8b02422c55d
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-05-10 12:40:58 -0700

    deadend

--- system.ts
+++ system.ts
@@ -1,5 +1,4 @@
-export class System<TSchema extends SystemSchema> // T extends { [service: string]: { [action: string]: [unknown, unknown] } },
-{
+export class System<TSchema extends SystemSchema> {
   constructor(
     private readonly services: ServicesMap<TSchema> = new Map(),
   ) {}
@@ -32,12 +31,20 @@ export class System<TSchema extends SystemSchema> // T extends { [service: strin
   /**
    * from creates a new system from a list of services.
    */
-  static from<TSchema extends SystemSchema>(
-    services: ServicesRouter<TSchema>,
-  ): System<TSchema> {
-    return new System(new Map(Object.entries(services)));
+  static from<TRouter extends ServicesRouter<any>>(
+    services: TRouter,
+  ): System<SchemaOf<TRouter>> {
+    return new System(
+      new Map(
+        Object.entries(services)
+          .map(([name, actions]) => [name, routerService(actions)]),
+      ),
+    );
   }
 }
+
+export type SchemaOf<TRouter extends ServicesRouter<SystemSchema>> =
+  TRouter extends ServicesRouter<infer TSchema> ? TSchema : never;
 
 /**
  * ServicesMap is a map of service names to services.
@@ -51,19 +58,48 @@ export type ServicesMap<TSchema extends SystemSchema> = Map<
  * ServicesRouter is a map of service names to services.
  */
 export type ServicesRouter<TSchema extends SystemSchema> = {
-  [serviceName in keyof TSchema]: {
-    [actionName in keyof TSchema[serviceName]]: ActionOf<
-      TSchema,
-      serviceName,
-      actionName
-    >;
-  };
+  [serviceName in keyof TSchema]: ActionsRouter<TSchema, serviceName>;
 };
+
+/**
+ * ActionsRouter is a map of action names to actions.
+ */
+export type ActionsRouter<
+  TSchema extends SystemSchema,
+  TService extends keyof TSchema,
+> = {
+  [actionName in keyof TSchema[TService]]: Action<
+    RequestOf<TSchema, TService, actionName>,
+    ResponseOf<TSchema, TService, actionName>
+  >;
+};
+
+// /**
+//  * ServicesRouter is a map of service names to services.
+//  */
+// export type ServicesRouter<TSchema extends SystemSchema> = {
+//   [serviceName in keyof TSchema]: {
+//     [actionName in keyof TSchema[serviceName]]: ActionOf<
+//       TSchema,
+//       serviceName,
+//       actionName
+//     >;
+//   };
+// };
 
 /**
  * SystemSchema is the schema of a system.
  */
-export type SystemSchema = Record<string, Record<string, [unknown, unknown]>>;
+export interface SystemSchema {
+  [serviceName: string]: ServiceSchema;
+}
+
+/**
+ * ServiceSchema is the schema of a service.
+ */
+export interface ServiceSchema {
+  [actionName: string]: [unknown, unknown];
+}
 
 /**
  * Action is a function that executes a service action.
@@ -80,8 +116,38 @@ export type ActionOf<
   TService extends keyof TSchema,
   TAction extends keyof TSchema[TService],
 > = (
+  // TODO: Figure out how to pass generic request/response types when defining the action.
   ctx: ActionContextOf<TSchema, TService, TAction>,
 ) => ResponseOf<TSchema, TService, TAction>;
+
+/**
+ * Action is a function that executes a service action in a system.
+ */
+export interface Action<TRequest, TResponse> {
+  (ctx: ActionContext<TRequest>): TResponse;
+}
+
+/**
+ * routerService makes a service from an actions router.
+ */
+export function routerService<
+  TSchema extends SystemSchema,
+  TService extends keyof TSchema,
+>(router: ActionsRouter<TSchema, TService>): Service<TSchema> {
+  return {
+    execute<TService_ extends TService, TAction_ extends TSchema[TService_]>(
+      ctx: ActionContextOf<TSchema, TService_, TAction_>,
+    ): ResponseOf<TSchema, TService, TAction> {
+      const actionName = String(ctx.actionName);
+      const action = router[actionName];
+      if (action === undefined) {
+        throw new Error(`Action not found: ${actionName}`);
+      }
+
+      return action(ctx as ActionContext<any>);
+    },
+  };
+}
 
 /**
  * Service executes actions.
@@ -93,19 +159,6 @@ export interface Service<TSchema extends SystemSchema> {
   >(
     ctx: ActionContextOf<TSchema, TService, TAction>,
   ): ResponseOf<TSchema, TService, TAction>;
-}
-
-/**
- * ActionInput is the input to execute an action at the system level.
- */
-export interface ActionInput<
-  TSchema extends SystemSchema,
-  TService extends keyof TSchema,
-  TAction extends keyof TSchema[TService],
-> {
-  readonly serviceName: TService;
-  readonly actionName: TAction;
-  readonly request: RequestOf<TSchema, TService, TAction>;
 }
 
 export type RequestOf<
@@ -133,10 +186,75 @@ export interface ActionContextOf<
   TSchema extends SystemSchema,
   TService extends keyof TSchema,
   TAction extends keyof TSchema[TService],
-> extends ActionInput<TSchema, TService, TAction> {
+> extends ActionInputOf<TSchema, TService, TAction> {
   readonly system: System<TSchema>;
   readonly service: Service<TSchema>;
 }
+
+/**
+ * ActionInput is the input to execute an action at the system level.
+ */
+export interface ActionInputOf<
+  TSchema extends SystemSchema,
+  TService extends keyof TSchema,
+  TAction extends keyof TSchema[TService],
+> {
+  readonly serviceName: TService;
+  readonly actionName: TAction;
+  readonly request: RequestOf<TSchema, TService, TAction>;
+}
+
+// /**
+//  * ActionInput is the input to execute an action at the system level.
+//  */
+// export interface ActionInput<
+//   TSchema extends SystemSchema,
+//   TService extends keyof TSchema,
+//   TAction extends keyof TSchema[TService],
+//   TRequest extends RequestOf<TSchema, TService, TAction> = RequestOf<
+//     TSchema,
+//     TService,
+//     TAction
+//   >,
+// > {
+//   readonly serviceName: TService;
+//   readonly actionName: TAction;
+//   readonly request: TRequest;
+// }
+
+/**
+ * ActionInput is the input to execute an action at the system level.
+ */
+export interface ActionInput<TRequest> {
+  readonly serviceName: string;
+  readonly actionName: string;
+  readonly request: TRequest;
+}
+
+/**
+ * ActionContext is the context passed to a service action.
+ */
+export interface ActionContext<TRequest> extends ActionInput<TRequest> {
+  readonly system: System<any>;
+  readonly service: Service<any>;
+}
+
+// /**
+//  * ActionContext is the context passed to a service action.
+//  */
+// export interface ActionContextOf<
+//   TSchema extends SystemSchema,
+//   TService extends keyof TSchema,
+//   TAction extends keyof TSchema[TService],
+//   TRequest extends RequestOf<TSchema, TService, TAction> = RequestOf<
+//     TSchema,
+//     TService,
+//     TAction
+//   >,
+// > extends ActionInput<TSchema, TService, TAction, TRequest> {
+//   readonly system: System<TSchema>;
+//   readonly service: Service<TSchema>;
+// }
 
 // export interface Item {
 //   schema: Record<string, string>;
--- system_example.ts
+++ system_example.ts
@@ -1,32 +1,16 @@
 // import type { ActionContext } from "./system.ts";
-import { System } from "./system.ts";
+import { ActionContext, System } from "./system.ts";
 
 if (import.meta.main) {
-  const system = System.from({
+  const router = {
     greeting: {
-      // Type '(ctx: ActionContextOf<SystemSchema, "greeting", "greet">) => string' is not assignable to type 'ActionOf<SystemSchema, string, string>'.
-      greet(ctx): string {
+      greet(ctx: ActionContext<string>) {
         return `Hello, ${ctx.request}!`;
       },
     },
-    // items: {
-    //   execute<TIn, TOut>(ctx: ActionContext<TIn>): TOut {
-    //     switch (ctx.actionName) {
-    //       case "get": {
-    //         return ctx.request as unknown as TOut;
-    //       }
+  } as const;
 
-    //       case "set": {
-    //         return ctx.request as unknown as TOut;
-    //       }
-
-    //       default: {
-    //         throw new Error(`Action not found: ${ctx.actionName}`);
-    //       }
-    //     }
-    //   },
-    // },
-  });
+  const system = System.from(router);
 
   const result = system.execute("items", "get", "hello");
   console.log(result);
