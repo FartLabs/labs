0be6cff6005bec020a5dab5228dd8c17386776df
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-05-12 12:01:41 -0700

    Update system_example.ts

--- system.ts
+++ system.ts
@@ -1,49 +1,67 @@
-export type ServiceSchemaOf<TService> = TService extends Service<infer TSchema>
-  ? TSchema
-  : never;
+/**
+ * createService creates a service from a schema of actions.
+ */
+export function createService<TSchema extends ServiceSchema>(
+  actions: ContextOmitService<TSchema> extends Record<PropertyKey, never>
+    ? TSchema
+    : never,
+): Service<TSchema>;
+export function createService<TSchema extends ServiceSchema>(
+  actions: TSchema,
+  ctx: ContextOmitService<TSchema>,
+): Service<TSchema>;
+export function createService<TSchema extends ServiceSchema>(
+  actions: TSchema,
+  ctx?: ContextOmitService<TSchema>, // = {} as ContextOmitService<TSchema>,
+): Service<TSchema> {
+  const service = Object.fromEntries(
+    Object.entries(actions).map(([actionName, action]) => [
+      actionName,
+      (request: Parameters<typeof action>[0]) =>
+        action(request, { ...service, ...(ctx ?? {}) }),
+    ]),
+  ) as Service<TSchema>;
 
-export class Service<TSchema extends ServiceSchema> {
-  public constructor(
-    private readonly actions: TSchema,
-  ) {}
-
-  public execute<TAction extends keyof TSchema>(
-    actionName: TAction,
-    ctx: Parameters<TSchema[TAction]>[0],
-  ): ReturnType<TSchema[TAction]> {
-    return this.actions[actionName](ctx);
-  }
+  return service;
 }
 
-export interface SystemSchema {
-  [serviceName: string]: ServiceSchema;
+export type ContextWithService<TSchema extends ServiceSchema> =
+  & ContextOf<TSchema>
+  & Service<TSchema>;
+
+export type ContextOmitService<TSchema extends ServiceSchema> = Omit<
+  ContextOf<TSchema>,
+  keyof TSchema
+>;
+
+export type ContextOf<TSchema extends ServiceSchema> = ContextSchemaOf<
+  TSchema
+>[keyof TSchema];
+
+export type ContextSchemaOf<TSchema extends ServiceSchema> = {
+  [actionName in keyof TSchema]: TSchema[actionName] extends Action
+    ? Parameters<TSchema[actionName]>[1]
+    : never;
+};
+
+export type Service<TSchema extends ServiceSchema> = {
+  [actionName in keyof TSchema]: ServiceActionOf<TSchema[actionName]>;
+};
+
+export type ServiceSchema = Record<string, Action>;
+
+export type ServiceActionOf<T extends Action> = Parameters<T>[0] extends
+  RequestType.EMPTY ? () => ReturnType<T>
+  : (request: Parameters<T>[0]) => ReturnType<T>;
+
+// deno-lint-ignore no-explicit-any
+export type Action = (request: any, ctx: any) => any;
+
+export type Context = Record<string, ServiceAction>;
+
+// deno-lint-ignore no-explicit-any
+export type ServiceAction = (request: any) => any;
+
+export enum RequestType {
+  EMPTY,
 }
-
-export interface ServiceSchema {
-  // deno-lint-ignore no-explicit-any
-  [actionName: string]: Action<any, any>;
-}
-
-export interface Action<TContext, TResponse> {
-  (ctx: TContext): TResponse;
-}
-
-// export interface ServiceActionContext<
-//   TRequest,
-//   TService extends typeof Service,
-// > extends ActionContext<TRequest> {
-//   service: TService;
-// }
-
-export type ActionContext<
-  TRequest = never,
-  TSchema extends SystemSchema = never,
-> =
-  & { request: TRequest }
-  & {
-    services: { [serviceName in keyof TSchema]: Service<TSchema[serviceName]> };
-  };
-
-// & (TSchema extends never ? {} : {
-//   services: { [serviceName in keyof TSchema]: Service<TSchema[serviceName]> };
-// });
--- system_example.ts
+++ system_example.ts
@@ -1,73 +1,5 @@
-/**
- * createService creates a service from a schema of actions.
- */
-function createService<TSchema extends ServiceSchema>(
-  actions: ContextOmitService<TSchema> extends Record<PropertyKey, unknown>
-    ? TSchema
-    : never,
-): Service<TSchema>;
-function createService<TSchema extends ServiceSchema>(
-  actions: TSchema,
-  ctx: ContextOmitService<TSchema>,
-): Service<TSchema>;
-function createService<TSchema extends ServiceSchema>(
-  actions: TSchema,
-  ctx?: ContextOmitService<TSchema>, // = {} as ContextOmitService<TSchema>,
-): Service<TSchema> {
-  const service = Object.fromEntries(
-    Object.entries(actions).map(([actionName, action]) => [
-      actionName,
-      (request: Parameters<typeof action>[0]) =>
-        action(request, { ...service, ...(ctx ?? {}) }),
-    ]),
-  ) as Service<TSchema>;
-
-  return service;
-}
-
-// If action depends on action from local service and not external context
-// then we can do this.
-
-export type ContextWithService<TSchema extends ServiceSchema> =
-  & ContextOf<TSchema>
-  & Service<TSchema>;
-
-export type ContextOmitService<TSchema extends ServiceSchema> = Omit<
-  ContextOf<TSchema>,
-  keyof TSchema
->;
-
-export type ContextOf<TSchema extends ServiceSchema> = ContextSchemaOf<
-  TSchema
->[keyof TSchema];
-
-export type ContextSchemaOf<TSchema extends ServiceSchema> = {
-  [actionName in keyof TSchema]: TSchema[actionName] extends Action
-    ? Parameters<TSchema[actionName]>[1]
-    : never;
-};
-
-export type Service<TSchema extends ServiceSchema> = {
-  [actionName in keyof TSchema]: ServiceActionOf<TSchema[actionName]>;
-};
-
-export type ServiceSchema = Record<string, Action>;
-
-export type ServiceActionOf<T extends Action> = Parameters<T>[0] extends
-  RequestType.EMPTY ? () => ReturnType<T>
-  : (request: Parameters<T>[0]) => ReturnType<T>;
-
-// deno-lint-ignore no-explicit-any
-export type Action = (request: any, ctx: any) => any;
-
-export type Context = Record<string, ServiceAction>;
-
-// deno-lint-ignore no-explicit-any
-export type ServiceAction = (request: any) => any;
-
-export enum RequestType {
-  EMPTY,
-}
+import type { RequestType, ServiceActionOf } from "./system.ts";
+import { createService } from "./system.ts";
 
 function greet(
   request: { name: string },
@@ -92,8 +24,6 @@ function randomFruit(
 ): string {
   return ctx.pick({ from: "🍎🍊🍌🍉".split("") });
 }
-
-type GreetContext = ContextOmitService<{ greet: typeof greet }>;
 
 if (import.meta.main) {
   const randomService = createService({ randomFruit, pick });
