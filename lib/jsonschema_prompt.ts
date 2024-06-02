9effbf80b55e5c2c6192089b7f68689f3874cb08
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-02 14:16:43 -0700

    wip jsonschema prompt

---
+++ lib/jsonschema_prompt.ts
@@ -0,0 +1,7 @@
+import type * as types from "json-schema";
+
+export function jsonSchemaPrompt(schema: types) {
+  // TODO: Write prompt function that generates a prompt for a JSON schema.
+  // https://json-schema.org/blog/posts/get-started-with-json-schema-in-node-js
+  console.log("schema", schema);
+}
