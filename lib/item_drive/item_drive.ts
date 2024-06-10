e0175764ed6ebaa689e65fe4585275df970c6409
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-09 20:04:56 -0700

    add ItemDrive eventTarget private property
    - https://developer.mozilla.org/en-US/docs/Web/Events/Creating_and_triggering_events

--- lib/item_drive/item_drive.ts
+++ lib/item_drive/item_drive.ts
@@ -5,19 +5,69 @@ import { DataSource } from "../data_source/mod.ts";
  */
 export type ItemDriveSchema = Record<string, unknown>;
 
-// TODO: Create special item drive implementation for automatically updating properties of specific items such as created_at, updated_at, deleted_at, etc. Or common building blocks for items such as counters, etc.
+/**
+ * ItemDriveEventType is an event type of an item drive.
+ */
+export type ItemDriveEventType = "set" | "get" | "delete";
+
+/**
+ * ItemDriveEventDetail is a detail of an item drive event.
+ */
+export interface ItemDriveEventDetail<T> {
+  collection: string;
+  name: string;
+  item?: T;
+}
 
 export class ItemDrive<TItemDriveSchema extends ItemDriveSchema> {
+  private readonly eventTarget = new EventTarget();
+
   public constructor(private readonly dataSource: DataSource) {}
 
+  /**
+   * dispatch dispatches an item drive event.
+   */
+  private dispatch(
+    type: ItemDriveEventType,
+    detail: ItemDriveEventDetail<TItemDriveSchema[keyof TItemDriveSchema]>,
+  ) {
+    this.eventTarget.dispatchEvent(new CustomEvent(type, { detail }));
+    this.eventTarget.dispatchEvent(
+      new CustomEvent(`${type}:${detail.collection}`, { detail }),
+    );
+    this.eventTarget.dispatchEvent(
+      new CustomEvent(
+        `${type}:${detail.collection}:${detail.name}`,
+        { detail },
+      ),
+    );
+  }
+
+  /**
+   * addEventListener adds an event listener.
+   */
+  public addEventListener(
+    type: ItemDriveEventType,
+    fn: (event: Event) => void,
+  ) {
+    this.eventTarget.addEventListener(type, fn);
+  }
+
+  /**
+   * setItem sets an item.
+   */
   public async setItem<TCollection extends keyof TItemDriveSchema>(
     collection: TCollection,
     name: string,
     item: TItemDriveSchema[TCollection],
   ) {
     await this.dataSource.setItem(collection.toString(), name, item);
+    this.dispatch("set", { collection: collection.toString(), name, item });
   }
 
+  /**
+   * setItems sets items.
+   */
   public async setItems<TCollection extends keyof TItemDriveSchema>(
     collection: TCollection,
     items: Array<[string, TItemDriveSchema[TCollection]]>,
@@ -25,16 +75,27 @@ export class ItemDrive<TItemDriveSchema extends ItemDriveSchema> {
     await this.dataSource.setItems(collection.toString(), items);
   }
 
-  public getItem<TCollection extends keyof TItemDriveSchema>(
+  /**
+   * getItem gets an item.
+   */
+  public async getItem<TCollection extends keyof TItemDriveSchema>(
     collection: TCollection,
     name: string,
   ) {
-    return this.dataSource.getItem<string, TItemDriveSchema[TCollection]>(
+    const item = await this.dataSource.getItem<
+      string,
+      TItemDriveSchema[TCollection]
+    >(
       collection.toString(),
       name,
     );
+    this.dispatch("get", { collection: collection.toString(), name, item });
+    return item;
   }
 
+  /**
+   * getItems gets items.
+   */
   public getItems<TCollection extends keyof TItemDriveSchema>(
     collection: TCollection,
   ) {
@@ -43,16 +104,23 @@ export class ItemDrive<TItemDriveSchema extends ItemDriveSchema> {
     );
   }
 
-  public deleteItem<TCollection extends keyof TItemDriveSchema>(
+  /**
+   * deleteItem deletes an item.
+   */
+  public async deleteItem<TCollection extends keyof TItemDriveSchema>(
     collection: TCollection,
     name: string,
   ) {
-    this.dataSource.deleteItem(collection.toString(), name);
+    await this.dataSource.deleteItem(collection.toString(), name);
+    this.dispatch("delete", { collection: collection.toString(), name });
   }
 
+  /**
+   * deleteItems deletes items.
+   */
   public deleteItems<TCollection extends keyof TItemDriveSchema>(
     collection: TCollection,
   ) {
-    this.dataSource.deleteItems(collection.toString());
+    return this.dataSource.deleteItems(collection.toString());
   }
 }
