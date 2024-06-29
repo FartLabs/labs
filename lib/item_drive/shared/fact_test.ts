153fe67a15f4927606a9c0df8224a910b877987e
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-28 21:35:06 -0700

    add fact_test.ts file

---
+++ lib/item_drive/shared/fact_test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "@std/assert";
+import { makeFact } from "./fact.ts";
+
+Deno.test("makeFact makes a fact with a timestamp", () => {
+    const fact = makeFact({ attribute: "name", value: ["Ethan"] });
+    assertEquals(fact.attribute, "name");
+});
