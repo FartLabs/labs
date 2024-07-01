06cd2433fecdfa8aa607271a18b50a5fd428e267
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-01 13:47:00 -0700

    move function `checkFact` to fact.ts

--- lib/item_drive/shared/fact_test.ts
+++ lib/item_drive/shared/fact_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals, assertThrows } from "@std/assert";
-import { makeFact } from "./fact.ts";
+import { checkFact, makeFact } from "./fact.ts";
 
 Deno.test("makeFact throws error if attribute is missing", () => {
   assertThrows(() => makeFact({ value: ["Ethan"] }));
@@ -11,4 +11,10 @@ Deno.test("makeFact makes a fact from an attribute and value", () => {
   assertEquals(fact.attribute, "name");
   assertEquals(fact.value, ["Ethan"]);
   assertEquals(fact.timestamp, date.getTime());
+});
+
+Deno.test("checkFact returns true if fact matches query", () => {
+  const fact = makeFact({ attribute: "name", value: ["Ethan"] });
+  const query = { attributes: [{ attribute: "name" }] };
+  assertEquals(checkFact(fact, query), true);
 });
