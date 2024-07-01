803d0b6b17b29d1cdcc04f111eeff87ecc5b2b55
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-01 13:53:36 -0700

    wip: function `checkFact` tests

--- lib/item_drive/shared/fact_test.ts
+++ lib/item_drive/shared/fact_test.ts
@@ -13,8 +13,38 @@ Deno.test("makeFact makes a fact from an attribute and value", () => {
   assertEquals(fact.timestamp, date.getTime());
 });
 
-Deno.test("checkFact returns true if fact matches query", () => {
+Deno.test("checkFact returns true if fact matches query by attribute", () => {
   const fact = makeFact({ attribute: "name", value: ["Ethan"] });
   const query = { attributes: [{ attribute: "name" }] };
+  assertEquals(checkFact(fact, query), true);
+});
+
+Deno.test("checkFact returns false if fact does not match query by attribute", () => {
+  const fact = makeFact({ attribute: "name", value: ["Ethan"] });
+  const query = { attributes: [{ attribute: "age" }] };
+  assertEquals(checkFact(fact, query), false);
+});
+
+Deno.test("checkFact returns true if fact matches query by value", () => {
+  const fact = makeFact({ attribute: "name", value: ["Ethan"] });
+  const query = {
+    attributes: [{ attribute: "name", valueIncludes: ["Ethan"] }],
+  };
+  assertEquals(checkFact(fact, query), true);
+});
+
+Deno.test("checkFact returns false if fact does not match query by value", () => {
+  const fact = makeFact({ attribute: "name", value: ["Ethan"] });
+  const query = {
+    attributes: [{ attribute: "name", valueIncludes: ["Not Ethan"] }],
+  };
+  assertEquals(checkFact(fact, query), false);
+});
+
+Deno.test("checkFact returns true if fact matches query by multiple values", () => {
+  const fact = makeFact({ attribute: "name", value: ["Ethan"] });
+  const query = {
+    attributes: [{ attribute: "name", valueIncludes: ["Ethan", "Not Ethan"] }],
+  };
   assertEquals(checkFact(fact, query), true);
 });
