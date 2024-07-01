4dcb2031c361aad472e7f4a097c47e9a1dc57182
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-01 12:25:35 -0700

    Update fact_test.ts

--- lib/item_drive/shared/fact_test.ts
+++ lib/item_drive/shared/fact_test.ts
@@ -1,47 +1,14 @@
 import { assertEquals, assertThrows } from "@std/assert";
 import { makeFact } from "./fact.ts";
 
+Deno.test("makeFact throws error if attribute is missing", () => {
+  assertThrows(() => makeFact({ value: ["Ethan"] }));
+});
+
 Deno.test("makeFact makes a fact from an attribute and value", () => {
-  const fact = makeFact({ attribute: "name", value: ["Ethan"] });
+  const date = new Date(0);
+  const fact = makeFact({ attribute: "name", value: ["Ethan"] }, date);
   assertEquals(fact.attribute, "name");
   assertEquals(fact.value, ["Ethan"]);
+  assertEquals(fact.timestamp, date.getTime());
 });
-
-Deno.test("makeFact makes a fact from an attribute and numerical value", () => {
-  const fact = makeFact({
-    attribute: "age",
-    type: "number",
-    numericalValue: [10],
-  });
-  assertEquals(fact.attribute, "age");
-  assertEquals(fact.numericalValue, [10]);
-});
-
-Deno.test("makeFact makes a fact with multiple values if repeatable is true", () => {
-  const fact = makeFact({
-    attribute: "name",
-    value: ["Ethan", "Ash"],
-    repeatable: true,
-  });
-  assertEquals(fact.attribute, "name");
-  assertEquals(fact.value, ["Ethan", "Ash"]);
-});
-
-Deno.test("makeFact throws if values mismatch the numericalValues", () => {
-  assertThrows(() => {
-    makeFact({ attribute: "age", value: ["Ash"], numericalValue: [10] });
-  });
-});
-
-Deno.test("makeFact infers the values of a numerical fact from given values", () => {
-  const fact = makeFact({
-    attribute: "age",
-    numericalValue: [10],
-    type: "number",
-  });
-  assertEquals(fact.attribute, "age");
-  assertEquals(fact.numericalValue, [10]);
-  assertEquals(fact.value, ["10"]);
-});
-
-// TODO: Write a list of tests for the makeFact function.
